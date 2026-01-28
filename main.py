b30e825de8d803a7079a1cde62ea2b349f9a021c
Guthyers Oliveira <guthyerss@gmail.com>
2026-01-28 20:38:48 -0300

    Adicionada a função excluir, para remover itens do arquivo json

--- main.py
+++ main.py
@@ -38,9 +38,33 @@ class Gerenciamento:
             json.dump(self.dados, arquivo, indent=4, ensure_ascii=False)
 
         return "item adicionado com sucesso"
+    
+    def excluir(self, nome_procurado):
+        #1° percorrer a lista (sim, esse arquivo é uma LISTA DE DISCIONARIOS)
+        item_para_remover = None
+        for item in self.dados:
+             if item.get('item') == nome_procurado:
+                 item_para_remover = item
+                 break
+
+
+
+        #2° remover item do discinario 
+        if item_para_remover:
+            self.dados.remove(item_para_remover)
+
+            with open ("inventario.json", "w", encoding="utf-8") as arquivo: # w para gravação
+                json.dump(self.dados, arquivo, indent=4, ensure_ascii=False)
+            
+            print(f"item {nome_procurado} foi removido com sucesso!")
+            return item_para_remover
+        else:
+           print("item não encontrado")
+           return None
+
 
 cliente = Gerenciamento(arquivodedados) #cliente recebe a classe que instância arquivodedados, no caso o json   
 
-
+cliente.excluir("teste")
 #cliente.listar() #cliente chama a função listar 
 print(cliente.listar()) #exibe a lista
